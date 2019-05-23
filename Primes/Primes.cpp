ef530740f7d35dcda9cecdf8c92209e6c0f6df44
kuroteren <kuroteren@gmail.com>
2019-05-23 10:08:25 -0700

    Update Primes.cpp
    Functional prime finder

--- Primes/Primes.cpp
+++ Primes/Primes.cpp
@@ -3,10 +3,61 @@
 
 #include "pch.h"
 #include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+#define CLS "\033[2J\033[1;1H"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	unsigned long int x = 1, y, max;
+	vector<int> listing;
+	bool prime = true;
+
+	do {
+		cout << "Enter max number to test up to (Max number: " << ULONG_MAX << "): ";
+		cin >> max;
+	} while (max == 0);
+
+	for (x; x <= max ; x++)
+	{
+		prime = true;
+
+		cout << CLS;
+
+		cout << "Completed: " << (double)((x / max)*100) << "%" << endl;
+
+		for (auto it = listing.begin(); it != listing.end(); it++)
+		{
+			if (listing.size() == 0)
+			{
+				prime = true;
+				break;
+			}
+
+			y = x % *it;
+
+			if (y == 0 && *it != 1)
+			{
+				prime = false;
+				break;
+			}
+		}
+
+		if (prime)
+		{
+			listing.push_back(x);
+		}
+	}
+
+	cout << "The primes within limit(1::" << max << ") are:" << endl;
+
+	for (auto it = listing.begin(); it != listing.end(); it++)
+	{
+		cout << *it << ", ";
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
